caca367d5de090048553d30df643e089d7f6170d
Kelly(MaEunKyoung) <unmelted@naver.com>
2022-08-30 11:58:52 +0900

    misspel debug

--- backend/auto_calib_back.config.js
+++ backend/auto_calib_back.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: 'auto_calib_back',
         script: './bin/www',
-        instances: 0,
+        instances: 8,
         exec_mode: 'cluster',
         autorestart: true,
         watch: true,
@@ -10,9 +10,9 @@ module.exports = {
         max_memory_restart: '200M',
         wait_ready: true,
         listen_timeout: 50000,
-        log_date_format : "YY-MM-DD HH:mm:ss Z",
-        error_file : "./logs/auto_calib_back.log",
-        out_file : "./logs/auto_calib_back.log",
+        log_date_format: "YY-MM-DD HH:mm:ss Z",
+        error_file: "./logs/auto_calib_back.log",
+        out_file: "./logs/auto_calib_back.log",
         merge_logs: true
     }]
 }
